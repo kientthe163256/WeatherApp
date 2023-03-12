f805efcfea22c92184a4c3edf44389def3870f57
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2023-03-12 21:04:28 +0700

    feat(mock): add daily weather mock

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -12,6 +12,13 @@ import android.location.Location;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -31,6 +38,7 @@ import com.example.weatherapp.dao.DailyWeatherDao;
 import com.example.weatherapp.dao.HourlyWeatherDao;
 import com.example.weatherapp.dao.LocationDao;
 import com.example.weatherapp.model.AppLocation;
+import com.example.weatherapp.model.DailyWeather;
 import com.example.weatherapp.model.HourlyWeather;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
@@ -43,7 +51,12 @@ import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.Priority;
 import com.google.android.gms.tasks.Task;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import org.json.JSONArray;
@@ -101,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
                 user not allow -> get default appLocation
         * */
         checkLocationPermission();
+        List<DailyWeather> dailyWeathers = mockDailyWeatherList();
+        setUpDailyWeather(dailyWeathers);
     }
 
     public boolean hasInternetConnection() {
@@ -335,5 +350,99 @@ public class MainActivity extends AppCompatActivity {
         private void deleteHourlyWeatherByLocationId(int locationId) {
             hourlyWeatherDao.deleteByLocationId(locationId);
         }
+    }
+
+    private List<DailyWeather> mockDailyWeatherList() {
+        List<DailyWeather> weatherList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DailyWeather weather = new DailyWeather();
+            weather.setTime(Instant.now().plus(i, ChronoUnit.DAYS).toEpochMilli());
+            weather.setLastUpdate(Instant.now().toEpochMilli());
+            weather.setMinTemp(getRandomNumber(15, 20));
+            weather.setMaxTemp(weather.getMinTemp() + getRandomNumber(3, 8));
+            if (getRandomNumber(0, 3) != 0) {
+                weather.setWeather("Sunny");
+            } else {
+                weather.setWeather("Cloudy");
+            }
+            weatherList.add(weather);
+        }
+        return weatherList;
+    }
+
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    private void setUpDailyWeather(List<DailyWeather> dailyWeathers) {
+        TableLayout tableLayout = findViewById(R.id.daily_weather);
+        LayoutInflater inflater = getLayoutInflater();
+        int maxTemp = getMaxTemp(dailyWeathers);
+        int minTemp = getMinTemp(dailyWeathers);
+        int maxTempDiff = maxTemp - minTemp;
+        for (DailyWeather dailyWeather : dailyWeathers) {
+            TableRow row = inflater.inflate(R.layout.daily_weather_layout, null,
+                    false)
+                .findViewById(R.id.row_day);
+            TextView date = row.findViewById(R.id.row_day_day);
+            String dateStr = getDayOfWeek("EEEE", new Date(dailyWeather.getTime()));
+            date.setText(dateStr);
+            ImageView icon = row.findViewById(R.id.row_day_icon);
+            HashMap<String, Integer> iconMap = getIconMap();
+            iconMap.put("Cloudy", R.drawable.cloudy);
+            iconMap.put("Sunny", R.drawable.sun);
+            icon.setImageResource(
+                iconMap.getOrDefault(dailyWeather.getWeather(), R.drawable.cloudy));
+
+            View tempDiff = row.findViewById(R.id.row_day_range_fg);
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) tempDiff.getLayoutParams();
+            params.leftMargin = dpToPx(
+                (int) ((dailyWeather.getMinTemp() - minTemp) / maxTempDiff * 100));
+            int width = dpToPx(
+                (int) ((dailyWeather.getMaxTemp() - dailyWeather.getMinTemp())
+                    / maxTempDiff * 100));
+            params.width = dpToPx(width);
+            tempDiff.setLayoutParams(params);
+
+            TextView minTempView = row.findViewById(R.id.row_day_min_temp);
+            minTempView.setText(String.valueOf((int) dailyWeather.getMinTemp()));
+
+            TextView maxTempView = row.findViewById(R.id.row_day_max_temp);
+            maxTempView.setText(String.valueOf((int) dailyWeather.getMaxTemp()));
+            tableLayout.addView(row);
+        }
+    }
+
+    private int dpToPx(int dp) {
+        return dp * (getResources().getDisplayMetrics().densityDpi
+            / DisplayMetrics.DENSITY_DEFAULT);
+    }
+
+    private int getMaxTemp(List<DailyWeather> dailyWeathers) {
+        return dailyWeathers.stream().map(
+                dailyWeather -> (int) dailyWeather.getMaxTemp()).max(Integer::compareTo)
+            .orElse(0);
+    }
+
+    private int getMinTemp(List<DailyWeather> dailyWeathers) {
+        return dailyWeathers.stream().map(
+                dailyWeather -> (int) dailyWeather.getMinTemp()).min(Integer::compareTo)
+            .orElse(0);
+    }
+
+    private HashMap<String, Integer> getIconMap() {
+        HashMap<String, Integer> iconMap = new HashMap<>();
+        iconMap.put("Sunny", R.drawable.sun);
+        iconMap.put("Cloudy", R.drawable.cloudy);
+        return iconMap;
+    }
+
+    private String getDayOfWeek(String format, Date date) {
+        return new SimpleDateFormat(format, Locale.ENGLISH).format(
+            date);
+    }
+
+    private String currentHourAndMinute() {
+        return getDayOfWeek("HH:mm", new Date());
     }
 }
